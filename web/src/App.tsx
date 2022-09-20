6d17b578c9dcd045fbaaba476f9a7b388462e129
IgorSilvaZZ <igor06silva12@gmail.com>
2022-09-19 21:28:34 -0300

    Landing page finish

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,5 @@
+import { MagnifyingGlassPlus } from "phosphor-react";
+
 import logoImg from "./assets/logo-nlw.svg";
 import image1 from "./assets/game-1.png";
 import image2 from "./assets/game-2.png";
@@ -80,7 +82,23 @@ function App() {
         </a>
       </div>
 
-      <div className='bg-[#2A2634] px-8 py-6 self-stretch mt-8 rounded-lg'></div>
+      <div className='pt-1 mt-8 bg-nlw-gradient self-stretch rounded-lg overflow-hidden'>
+        <div className='bg-[#2A2634] px-8 py-6 flex justify-between items-center'>
+          <div>
+            <strong className='text-2xl text-white font-black'>
+              Não encontrou seu dou?
+            </strong>
+            <span className='text-zinc-400 block'>
+              Publique um anúncio para encontrar novos players!
+            </span>
+          </div>
+
+          <button className='py-4 px-4 bg-violet-500 text-white rounded hover:bg-violet-600 flex items-center gap-3'>
+            <MagnifyingGlassPlus size={24} />
+            Publicar anuncio
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
