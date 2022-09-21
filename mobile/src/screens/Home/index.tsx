f1a7a9d6705e4c35255c886825ba3dca64c93a87
IgorSilvaZZ <igor06silva12@gmail.com>
2022-09-20 22:21:57 -0300

    Landing page mobile finish

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,8 +1,33 @@
-import React from "react";
-import { View } from "react-native";
+import { View, Image } from "react-native";
+
+import { FlatList } from "react-native";
+
+import logoImg from "../../assets/logo-nlw-esports.png";
+import { GameCard } from "../../components/GameCard";
+import { Header } from "../../components/Header";
+
+import { GAMES } from "../../utils/games";
 
 import { styles } from "./styles";
 
 export function Home() {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <Image source={logoImg} style={styles.logo} />
+
+      <Header
+        title='Encontre seu duo!'
+        subtitle='Selecione o game que deseja jogar...'
+      />
+
+      <FlatList
+        contentContainerStyle={styles.contentList}
+        data={GAMES}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <GameCard data={item} />}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+      />
+    </View>
+  );
 }
