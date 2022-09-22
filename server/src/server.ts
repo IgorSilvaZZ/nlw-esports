2596e4d8c42d91ae6d5f0d46d41c4a6ee629566b
IgorSilvaZZ <igor06silva12@gmail.com>
2022-09-21 22:29:38 -0300

    Initial server with prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,4 +2,22 @@ import express from "express";
 
 const app = express();
 
+app.get("/games", (req, res) => {
+  return res.json([]);
+});
+
+app.post("/ads", (req, res) => {
+  return res.status(201).json([]);
+});
+
+app.get("/games/:id/ads", (req, res) => {
+  const { id } = req.params;
+
+  return res.json([]);
+});
+
+app.get("/ads/:id/discord", (req, res) => {
+  return res.json([]);
+});
+
 app.listen(3333, () => console.log("🎮 Server is Running!"));
