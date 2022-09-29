2344a724abd363ec6ba22167c9c896f696e0c103
IgorSilvaZZ <igor06silva12@gmail.com>
2022-09-29 15:59:54 -0300

    Add modal in web

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,12 @@
 import { useState, useEffect } from "react";
+import * as Dialog from "@radix-ui/react-dialog";
 
 import { GameBanner } from "./components/GameBanner";
 import { CreateAdBanner } from "./components/CreateAdBanner";
 
 import logoImg from "./assets/logo-nlw.svg";
-import image1 from "./assets/game-1.png";
-import image2 from "./assets/game-2.png";
-import image3 from "./assets/game-3.png";
-import image4 from "./assets/game-4.png";
-import image5 from "./assets/game-5.png";
-import image6 from "./assets/game-6.png";
+import { GameController } from "phosphor-react";
+import { Input } from "./components/Form/Input";
 
 interface Game {
   id: string;
@@ -55,7 +52,125 @@ function App() {
         ))}
       </div>
 
-      <CreateAdBanner />
+      <Dialog.Root>
+        <CreateAdBanner />
+
+        <Dialog.Portal>
+          <Dialog.Overlay className='bg-black/60 inset-0 fixed' />
+
+          <Dialog.Content className='fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-black/25'>
+            <Dialog.Title className='text-3xl font-black'>
+              Publique um anúncio
+            </Dialog.Title>
+
+            <form className='mt-8 flex flex-col gap-4'>
+              <div className='flex flex-col gap-2'>
+                <label className='font-semibold' htmlFor='game'>
+                  Qual o game?
+                </label>
+                <Input id='game' placeholder='Selecione que deseja jogar' />
+              </div>
+
+              <div className='flex flex-col gap-2'>
+                <label htmlFor='name'>Seu nome (ou nickname)</label>
+                <Input id='name' placeholder='Como te chamam dentro do game?' />
+              </div>
+
+              <div className='grid grid-cols gap-6'>
+                <div className='flex flex-col gap-2'>
+                  <label htmlFor='yearsPlaying'>Joga há quantos anos?</label>
+                  <Input id='yearsPlaying' placeholder='Tudo bem ser ZERO' />
+                </div>
+
+                <div className='flex flex-col gap-2'>
+                  <label htmlFor='discord'>Qual seu Discord</label>
+                  <Input id='discord' placeholder='Usuario #00000' />
+                </div>
+              </div>
+
+              <div className='flex gap-6'>
+                <div className='flex flex-col gap-2'>
+                  <label htmlFor='weekDays'>Quando costuma jogar</label>
+
+                  <div className='grid grid-cols-4 gap-2'>
+                    <button
+                      className='w-8 h-8 roundend bg-zinc-900'
+                      title='Domingo'
+                    >
+                      D
+                    </button>
+                    <button
+                      className='w-8 h-8 roundend bg-zinc-900'
+                      title='Segunda'
+                    >
+                      S
+                    </button>
+                    <button
+                      className='w-8 h-8 roundend bg-zinc-900'
+                      title='Terça'
+                    >
+                      T
+                    </button>
+                    <button
+                      className='w-8 h-8 roundend bg-zinc-900'
+                      title='Quarta'
+                    >
+                      Q
+                    </button>
+                    <button
+                      className='w-8 h-8 roundend bg-zinc-900'
+                      title='Quinta'
+                    >
+                      Q
+                    </button>
+                    <button
+                      className='w-8 h-8 roundend bg-zinc-900'
+                      title='Sexta'
+                    >
+                      S
+                    </button>
+                    <button
+                      className='w-8 h-8 roundend bg-zinc-900'
+                      title='Sabado'
+                    >
+                      S
+                    </button>
+                  </div>
+                </div>
+
+                <div className='flex flex-col gap-2 flex-1'>
+                  <label htmlFor='hourStart'>Qual horario do dia?</label>
+
+                  <div className='grid grid-cols-2 gap-2'>
+                    <Input type='time' id='hourStart' placeholder='De' />
+                    <Input type='hourEnd' id='hourStart' placeholder='Até' />
+                  </div>
+                </div>
+              </div>
+
+              <div className='mt-2 flex gap-2 text-sm '>
+                <Input type='checkbox' name='' placeholder='' />
+                Costumo me conectar no chat de voz
+              </div>
+
+              <footer className='mt-4 flex justify-end gap-4'>
+                <Dialog.Close
+                  type='button'
+                  className='bg-zinc-500 px-5 h-12 rounded-md font-semibold hover:bg-zinc-600'
+                >
+                  Cancelar
+                </Dialog.Close>
+                <button
+                  className='bg-violet-500 px-5 h-12 rounded-md font-semibold flex items-center gap-3 hover:bg-violet-600'
+                  type='submit'
+                >
+                  <GameController size={24} /> Enconrar Dou
+                </button>
+              </footer>
+            </form>
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
     </div>
   );
 }
